5f0367c1127d0ebf59b589fb54a6335e505ce9f7
jchck <hey@justinchick.com>
2016-02-17 13:03:18 +0200

    enqueue styles

--- wp-pesticide.php
+++ wp-pesticide.php
@@ -9,3 +9,15 @@ Author URI:  http://jstn.ch/ck
 License:     MIT
 License URI: https://opensource.org/licenses/MIT
 */
+
+namespace jchck\pesticide;
+
+function outline(){
+	wp_enqueue_style('outline', plugins_url( 'styles/css/wp-pesticide-outline.css', __FILE__ ), false, null);
+}
+function depth(){
+	wp_enqueue_style('depth', plugins_url( 'styles/css/wp-pesticide-depth.css', __FILE__ ), false, null);
+}
+
+add_action('wp_enqueue_scripts', __NAMESPACE__ . '\\outline');
+add_action('wp_enqueue_scripts', __NAMESPACE__ . '\\depth');

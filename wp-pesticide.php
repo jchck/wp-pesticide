fd57a8a0f09069936677aa43f9099c242b9fadc2
jchck <hey@justinchick.com>
2016-02-17 11:26:14 +0200

    header block

---
+++ wp-pesticide.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: WP Pesticide
+Plugin URI:  https://github.com/jchck/wp-pesticide
+Description: Add Pesticide.css to your WordPress theme for faster CSS debugging
+Version:     1.0
+Author:      Justin Chick
+Author URI:  http://jstn.ch/ck
+License:     MIT
+License URI: https://opensource.org/licenses/MIT
+*/

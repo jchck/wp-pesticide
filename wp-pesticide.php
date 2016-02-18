1ebb1e8d4a6820ee72961b2444f5516f8073c878
jchck <hey@justinchick.com>
2016-02-18 09:13:38 +0200

    add comments

--- wp-pesticide.php
+++ wp-pesticide.php
@@ -10,8 +10,20 @@ License:     MIT
 License URI: https://opensource.org/licenses/MIT
 */
 
+/**
+ *
+ * Namespace our PHP file to avoid collisions
+ * @link http://php.net/manual/en/language.namespaces.php
+ *
+ */
 namespace jchck\pesticide;
 
+/**
+ *
+ * Enqueue our stylesheets
+ * @uses https://developer.wordpress.org/reference/functions/wp_enqueue_style
+ *
+ */
 function outline(){
 	wp_enqueue_style('outline', plugins_url( 'styles/css/wp-pesticide-outline.css', __FILE__ ), false, null);
 }
@@ -19,5 +31,12 @@ function depth(){
 	wp_enqueue_style('depth', plugins_url( 'styles/css/wp-pesticide-depth.css', __FILE__ ), false, null);
 }
 
+/**
+ *
+ * Fire the enqueued styles, note the use of __NAMESPACE__ constant in the second parameter
+ * @uses https://developer.wordpress.org/reference/functions/add_action
+ * @link http://php.net/manual/en/language.namespaces.nsconstants.php 
+ *
+ */
 add_action('wp_enqueue_scripts', __NAMESPACE__ . '\\outline');
 add_action('wp_enqueue_scripts', __NAMESPACE__ . '\\depth');
